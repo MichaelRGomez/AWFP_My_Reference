003272c827c6068b5b21d8f5c772e27716b1e383
michael <2018118090@ub.edu.bz>
2022-11-26 22:54:43 -0600

    added routes.go

---
+++ backend/cmd/api/routes.go
@@ -0,0 +1,15 @@
+// Filename: MyReference/backend/cmd/api/routes.go
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() http.Handler {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+}
--- backend/go.mod
+++ backend/go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require golang.org/x/crypto v0.3.0
 
-require github.com/lib/pq v1.10.7 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+)
