6c27f8be45320f7912fc04b6971155e60ef68d1b
michael <2018118090@ub.edu.bz>
2022-12-09 11:02:54 -0600

    finished addeding custom code to the project

--- backend/cmd/api/references.go
+++ backend/cmd/api/references.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -39,10 +40,129 @@ func (app *application) createdReferenceHandler(w http.ResponseWriter, r *http.R
 
 	//Creating a location header for the new created resource / Reference
 	headers := make(http.Header)
-	headers.Set("Location", fmt.Sprintf("/v1/reference/%d", reference.ID))
+	headers.Set("Location", fmt.Sprintf("/v1/references/%d", reference.ID))
 
 	//writing the response
 	err = app.writeJSON(w, http.StatusCreated, envelope{"reference": reference}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
+
+// showReferenceHandler() retrieves a given reference when supplied with a reference id
+func (app *application) showReferenceHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	reference, err := app.models.Reference.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//writing the json response
+	err = app.writeJSON(w, http.StatusOK, envelope{"reference": reference}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+// updateReferenceHandler() allows a user to edit the name of a reference; for now
+func (app *application) updateReferenceHandler(w http.ResponseWriter, r *http.Request) {
+	//pulling the id from the json request
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	//trying to retrieve the reference from the database
+	reference, err := app.models.Reference.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//constructing a new version of the reference
+	var input struct {
+		Name *string `json:"name"`
+	}
+
+	//copying over the info from the edit request
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	//checking for updates
+	if input.Name != nil {
+		reference.Name = *input.Name
+	}
+
+	//validating the chagnes
+	v := validator.New()
+	if data.ValidateReference(v, reference); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	//updating the reference on the database
+	err = app.models.Reference.Update(reference)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//writing the new info to a get() response to display to the caller
+	err = app.writeJSON(w, http.StatusOK, envelope{"reference": reference}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+// deleteReferenceHandler() will delete a given reference depending on the id provided
+func (app *application) deleteReferenceHandler(w http.ResponseWriter, r *http.Request) {
+	//pulling the id from the json request
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	//attemping to delete the reference if the id exist on the database
+	err = app.models.Reference.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//providing a confirmation response to the user
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "reference sucessfully deleted"}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -11,9 +11,20 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	//default endpoints
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	//user related endpoints
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activationUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
-	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+
+	//MyReference related endpoints
+	router.HandlerFunc(http.MethodPost, "/v1/references", app.requirePermission("reference:write", app.createdReferenceHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/references/:id", app.requirePermission("reference:read", app.showReferenceHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/references/:id", app.requirePermission("reference:write", app.updateReferenceHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/references/:id", app.requirePermission("reference:write", app.deleteReferenceHandler))
+
+	//middleware chain
+	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
